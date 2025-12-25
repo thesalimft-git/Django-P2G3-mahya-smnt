c8f10f7a1458a35c3e2b354557595ea6cfa86582
sali mft <thesalimft@gmail.com>
2025-12-25 17:38:29 +0330

    add template for product

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,6 @@ class Product(models.Model):
     name = models.CharField(max_length=250)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     stock = models.IntegerField()
-    
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
